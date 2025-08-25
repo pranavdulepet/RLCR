f512e42b9352284c84b27b83f491901939f653e9
pdulepet <pdulepet@umd.edu>
2025-08-25 09:13:35 -0400

    Revert "modified to brtx compatibility, need to test"
    This reverts commit 0380e8d565f7cdad6d3cadb3f8887614f0b52f9d.

--- eval_runs.sh
+++ eval_runs.sh
@@ -1,21 +1,21 @@
-#HOTPOTQA Models  
-python evaluation.py --config eval_configs/Hotpot-models/commonsenseqa.json
-python evaluation.py --config eval_configs/Hotpot-models/gpqa.json
-python evaluation.py --config eval_configs/Hotpot-models/gsm8k.json
-python evaluation.py --config eval_configs/Hotpot-models/hotpot-eval-em.json
-python evaluation.py --config eval_configs/Hotpot-models/hotpot-vanilla-eval-em.json
-python evaluation.py --config eval_configs/Hotpot-models/math-500.json
-python evaluation.py --config eval_configs/Hotpot-models/simpleqa.json
-python evaluation.py --config eval_configs/Hotpot-models/trivia.json
+#HOTPOTQA Models
+CUDA_VISIBLE_DEVICES=0 python evaluation.py --config eval_configs/Hotpot-models/commonsenseqa.json
+CUDA_VISIBLE_DEVICES=0 python evaluation.py --config eval_configs/Hotpot-models/gpqa.json
+CUDA_VISIBLE_DEVICES=0 python evaluation.py --config eval_configs/Hotpot-models/gsm8k.json
+CUDA_VISIBLE_DEVICES=0 python evaluation.py --config eval_configs/Hotpot-models/hotpot-eval-em.json
+CUDA_VISIBLE_DEVICES=0 python evaluation.py --config eval_configs/Hotpot-models/hotpot-vanilla-eval-em.json
+CUDA_VISIBLE_DEVICES=0 python evaluation.py --config eval_configs/Hotpot-models/math-500.json
+CUDA_VISIBLE_DEVICES=0 python evaluation.py --config eval_configs/Hotpot-models/simpleqa.json
+CUDA_VISIBLE_DEVICES=0 python evaluation.py --config eval_configs/Hotpot-models/trivia.json
 
 #MATH Models 
-python evaluation.py --config eval_configs/Math-models/big-math-digits.json
-python evaluation.py --config eval_configs/Math-models/commonsenseqa.json
-python evaluation.py --config eval_configs/Math-models/gpqa.json
-python evaluation.py --config eval_configs/Math-models/gsm8k.json
-python evaluation.py --config eval_configs/Math-models/hotpot-vanilla-eval-em.json
-python evaluation.py --config eval_configs/Math-models/math-500.json
-python evaluation.py --config eval_configs/Math-models/simpleqa.json
-python evaluation.py --config eval_configs/Math-models/trivia.json
+CUDA_VISIBLE_DEVICES=0 python evaluation.py --config eval_configs/Math-models/big-math-digits.json
+CUDA_VISIBLE_DEVICES=0 python evaluation.py --config eval_configs/Math-models/commonsenseqa.json
+CUDA_VISIBLE_DEVICES=0 python evaluation.py --config eval_configs/Math-models/gpqa.json
+CUDA_VISIBLE_DEVICES=0 python evaluation.py --config eval_configs/Math-models/gsm8k.json
+CUDA_VISIBLE_DEVICES=0 python evaluation.py --config eval_configs/Math-models/hotpot-vanilla-eval-em.json
+CUDA_VISIBLE_DEVICES=0 python evaluation.py --config eval_configs/Math-models/math-500.json
+CUDA_VISIBLE_DEVICES=0 python evaluation.py --config eval_configs/Math-models/simpleqa.json
+CUDA_VISIBLE_DEVICES=0 python evaluation.py --config eval_configs/Math-models/trivia.json
 
 
